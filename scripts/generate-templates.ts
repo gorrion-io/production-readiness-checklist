cb4cf8703b41366dbcde1e8d851c4dc713bb726b
Dominik Guzy <dominik.guzy@gorrion.io>
2024-06-20 10:03:24 +0200

    mobile versions to releases

---
+++ scripts/generate-templates.ts
@@ -0,0 +1,5 @@
+import { getBaseTemplates } from "./lib/generate-base-templates";
+import { getMobileTemplates } from "./lib/generate-mobile-templates";
+
+getBaseTemplates();
+getMobileTemplates();
